e9349d17fb835eeffa502f367039d781ffe06cd5
Code Keper <aliswork1122@gmail.com>
2024-03-05 21:13:19 +0500

    first commit

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ try:
     with open(project_slides, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         subprocess.run(["cmd", "/c", "start", project_slides])
+
 except FileNotFoundError:
     print("error: file not found")
