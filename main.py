ed82bada5bbe382172ce96149111926e7671d2cf
Code Keper <aliswork1122@gmail.com>
2024-03-05 23:22:22 +0500

    final program

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import PyPDF2
-import subprocess
 
 try:
-    project_slides = "project_slides.pdf"
-    with open(project_slides, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        subprocess.run(["cmd", "/c", "start", project_slides])
+    # specifying the path of all files:
+    pdf1 = "pdf1.pdf"
+    pdf2 = "pdf2.pdf"
+    pdf3 = "pdf3.pdf"
+
+    # creating list of PDFs to merge:
+    all_pdfs = [pdf1, pdf2, pdf3]
+
+    # apply the merge function:
+    merger = PyPDF2.PdfMerger()
+
+    # iterate through each file in the list & merge:
+    for pdf in all_pdfs:
+        merger.append(pdf)
+    merger.write("merged.pdf")
+    merger.close()
 
 except FileNotFoundError:
     print("error: file not found")
